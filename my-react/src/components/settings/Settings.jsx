619b855b5b84d220e231d056e64eb435903f5fb9
jivv <rajaevberame21@gmail.com>
2025-08-08 14:51:50 +0800

    080825 settings

--- my-react/src/components/settings/Settings.jsx
+++ my-react/src/components/settings/Settings.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Option from "./Option";
-import Price from "./Price"
+import Price from "./Price";
 import { Link } from "react-router-dom";
-
 
 const Settings = ({
   brand,
@@ -18,63 +17,66 @@ const Settings = ({
   fetchSize,
   fetchBrand,
 }) => {
+  const role = localStorage.getItem("role");
+
   return (
     <div className="container ">
       <div className="options">
         <div className="h3 text-center">Options</div>
-      <div className="row my-5">
-
-        <div className="col card">
-          <div className="card-body">
-            <h4 className="card-title text-center">Brands</h4>
-            <Option
-              option={brand}
-              setOption={setBrand}
-              name={"brand"}
-              fetch={fetchBrand}
-            />
+        <div className="row my-5">
+          <div className="col card">
+            <div className="card-body">
+              <h4 className="card-title text-center">Brands</h4>
+              <Option
+                option={brand}
+                setOption={setBrand}
+                name={"brand"}
+                fetch={fetchBrand}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="col card">
-          <div className="card-body">
-            <h4 className="card-title text-center">Types</h4>
-            <Option
-              option={type}
-              setOption={setType}
-              name={"type"}
-              fetch={fetchType}
-            />
+          <div className="col card">
+            <div className="card-body">
+              <h4 className="card-title text-center">Types</h4>
+              <Option
+                option={type}
+                setOption={setType}
+                name={"type"}
+                fetch={fetchType}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="col card">
-          <div className="card-body">
-            <h4 className="card-title text-center">Colors</h4>
-            <Option
-              option={color}
-              setOption={setColor}
-              name={"color"}
-              fetch={fetchColor}
-            />
+          <div className="col card">
+            <div className="card-body">
+              <h4 className="card-title text-center">Colors</h4>
+              <Option
+                option={color}
+                setOption={setColor}
+                name={"color"}
+                fetch={fetchColor}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="col card">
-          <div className="card-body">
-            <h4 className="card-title text-center">Sizes</h4>
-            <Option
-              option={size}
-              setOption={setSize}
-              name={"size"}
-              fetch={fetchSize}
-            />
+          <div className="col card">
+            <div className="card-body">
+              <h4 className="card-title text-center">Sizes</h4>
+              <Option
+                option={size}
+                setOption={setSize}
+                name={"size"}
+                fetch={fetchSize}
+              />
+            </div>
           </div>
         </div>
       </div>
-      </div>
-      
-      <Link to="/register">Register Employee</Link>
+
+      {(role == 1 || role == 2) && (
+        <Link to="/register">Register Employee</Link>
+      )}
     </div>
   );
 };
