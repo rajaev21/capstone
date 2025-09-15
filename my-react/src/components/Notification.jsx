dca02d1125506f5175c3e122b7d39e647bf89b59
jivv <rajaevberame21@gmail.com>
2025-09-15 12:43:04 +0800

    150925

--- my-react/src/components/Notification.jsx
+++ my-react/src/components/Notification.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 const Notification = ({ inventory }) => {
   const paramID = new URLSearchParams(useLocation().search);
   const lowStocks = Array.isArray(inventory)
-    ? inventory.filter((item) => item.qty < 31)
+    ? inventory.filter((item) => item.qty < 5)
     : [];
   return (
     <ul className="navbar-nav me-3">
@@ -20,16 +20,16 @@ const Notification = ({ inventory }) => {
             {lowStocks.length > 0 && lowStocks.length}
           </span>
         </button>
-        <ul className="dropdown-menu ">
+        <ul className="dropdown-menu scrollable-dropdown">
           {lowStocks?.map((item, index) => {
             return (
-              <li key={index} className="nav-item">
+              <li key={index} className="dropdown-item">
                 <Link
                   className="nav-link text-dark"
                   onClick={() =>
                     setTimeout(() => {
                       window.location.reload();
-                    }, 500)
+                    }, 1000)
                   }
                   to={`/inventory?id=${item.id}`}
                 >
